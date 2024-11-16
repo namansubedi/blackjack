21b2cf50c044f573caf3e15a15b0ca5a90d023e2
namansubedi <naman.subedi12@gmail.com>
2024-11-16 14:33:03 -0600

    final readme update

--- BlackjackGame.java
+++ BlackjackGame.java
@@ -1,3 +1,25 @@
+/*
+This is a Blackjack game, created in Java as a part of the Foundations of Programming course at Lamar University.
+The team members for this project are Naman Subedi, Ronish Shrestha and Manmeet Kaur.
+
+This project requires Java to be installed on your machine.
+Check if Java is already installed: `java -version`. The minimum version needed is Java 8.
+If not installed, download and install it from the official Java website or through package managers like apt for Ubuntu, brew for macOS, etc.
+
+To run the program, use the following command:
+```
+javac BlackjackGame.java
+java BlackjackGame
+```
+
+In the 'cards' directory, 1.png to 52.png are card images Ace to King in the order Spades, Hearts, Diamonds, Clubs.
+Image 53.png is the back of the card. Image 54.png is the joker card.
+
+In the 'avatar' directory, p1.png to p12.png are the avatar images.
+
+You can find the full project at the link: https://github.com/namansubedi/blackjack
+*/
+
 // import swing for the GUI
 // import awt for the layout, and event for the event listeners
 import javax.swing.*;
